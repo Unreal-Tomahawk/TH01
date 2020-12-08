0977195cfbf6e0414b7202c548eb5c711143d60f
x3titan <x3reunion@yahoo.com>
2020-12-07 22:28:35 -0500

    升级为4.26版本并调试通过

--- Source/TH01/Private/TamMath.cpp
+++ Source/TH01/Private/TamMath.cpp
@@ -26,11 +26,11 @@ bool ATamMath::isPointInside2Points(FVector2D point1, FVector2D point2, FVector2
 	float dx = abs(point1.X - point2.X);
 	float dy = abs(point1.Y - point2.Y);
 	if (dx > dy) {
-		if (destPoint.X > std::max(point1.X, point2.X)) return false;
-		if (destPoint.X < std::min(point1.X, point2.X)) return false;
+		if (destPoint.X > __max(point1.X, point2.X)) return false;
+		if (destPoint.X < __min(point1.X, point2.X)) return false;
 	} else {
-		if (destPoint.Y > std::max(point1.Y, point2.Y)) return false;
-		if (destPoint.Y < std::min(point1.Y, point2.Y)) return false;
+		if (destPoint.Y > __max(point1.Y, point2.Y)) return false;
+		if (destPoint.Y < __min(point1.Y, point2.Y)) return false;
 	}
 	return true;
 }
